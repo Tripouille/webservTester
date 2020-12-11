ec7a53bcb2f3a6e5359400d3303507a008a3ba55
jgambard <jmichel.gambard@gmail.com>
2020-12-11 17:11:46 +0100

    add colors and checkStatus

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import client
 
+class colors:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    RESET = '\033[0m'
+
+def checkStatus(answer, status, reason):
+	print("Status: nginx: [{}] webserv: [{}]".format(status, answer.status), colors.GREEN + "OK" if answer.status == status else colors.RED + "KO", colors.RESET)
+
 serv = client.HTTPConnection('localhost:80')
 #serv.set_debuglevel(1);
-print('GET', '/')
-serv.request('GET', '/')
+headers = {"Host": ""}
+print('GET', '/', "no host")
+serv.request('GET', '/',None, headers)
 answer = serv.getresponse()
-print('Status: ', answer.status,' Reason: ', answer.reason)
-print("{}\n".format(answer.read()))
-
-print('GET', '/banana')
-serv.request('GET', '/banana')
+checkStatus(answer, 400, "OK")
+#print("{}\n".format(answer.read()))
+headers = {"Host": "localhost"}
+print('GET', '/', "valid host")
+serv.request('GET', '/',None, headers)
 answer = serv.getresponse()
-print('Status: ', answer.status,' Reason: ', answer.reason)
-print("{}\n".format(answer.read()))
+checkStatus(answer, 200, "OK")
+#print("{}\n".format(answer.read()))
 
 serv.close()
