d8f292142778a79699867824db6ea5f7c901af04
jgambard <jmichel.gambard@gmail.com>
2020-12-12 10:28:25 +0100

    add doRequest

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import client
 
+#Connection
+nginx = client.HTTPConnection('localhost:80')
+#nginx.set_debuglevel(1)
+webserv = client.HTTPConnection('localhost:8080')
+
 class colors:
     GREEN = '\033[92m'
     RED = '\033[91m'
     RESET = '\033[0m'
 
-def checkStatus(answer, status, reason):
-	print("Status: nginx: [{}] webserv: [{}]".format(status, answer.status), colors.GREEN + "OK" if answer.status == status else colors.RED + "KO", colors.RESET)
+def checkStatus(nginxAnswer, webservAnswer):
+	print("Status: nginx: [{}] webserv: [{}]".format(nginxAnswer.status, webservAnswer.status), colors.GREEN + "OK" if nginxAnswer.status == webservAnswer.status else colors.RED + "KO", colors.RESET)
 
-serv = client.HTTPConnection('localhost:80')
-#serv.set_debuglevel(1);
-headers = {"Host": ""}
-print('GET', '/', "no host")
-serv.request('GET', '/',None, headers)
-answer = serv.getresponse()
-checkStatus(answer, 400, "OK")
-#print("{}\n".format(answer.read()))
-headers = {"Host": "localhost"}
-print('GET', '/', "valid host")
-serv.request('GET', '/',None, headers)
-answer = serv.getresponse()
-checkStatus(answer, 200, "OK")
-#print("{}\n".format(answer.read()))
+def doRequest(method, path, body, headers):
+    nginx.request(method, path, body, headers)
+    nginxAnswer = nginx.getresponse()
+    webserv.request(method, path, body, headers)
+    webservAnswer = webserv.getresponse()
+    return nginxAnswer, webservAnswer
 
-serv.close()
+def getBody(nginxAnswer, webservAnswer):
+    return  nginxAnswer.read(), webservAnswer.read()
+
+#Test 1
+print('Test 1:', 'GET', '/', "no host")
+nginxAnswer, webservAnswer = doRequest('GET', '/', None, {"Host": ""})
+checkStatus(nginxAnswer, webservAnswer)
+getBody(nginxAnswer, webservAnswer)
+#Test 2
+print('Test 2:', 'GET', '/', "valid host")
+nginxAnswer, webservAnswer = doRequest('GET', '/', None, {"Host": "localhost"})
+checkStatus(nginxAnswer, webservAnswer)
+getBody(nginxAnswer, webservAnswer)
+
+#Close
+nginx.close()
+webserv.close()
